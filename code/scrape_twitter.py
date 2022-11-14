2cac1214ce59bfb9d39d84f324164ef80fcf2fd4
Tarini Sudhakar <tarinisudhakar@Tarinis-MacBook-Air-2.local>
2022-11-13 20:40:52 -0600

    Code for scraping econ_ra Twitter - need to check schedule and save into a Google Sheet

--- code/scrape_twitter.py
+++ code/scrape_twitter.py
@@ -1,1 +1,19 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import schedule
 
+def econ_ra():
+    attributes_container = []
+    
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:econ_ra include:nativeretweets').get_items()):
+        if i>200:
+            break
+    attributes_container.append([tweet.user.username, tweet.date, tweet.content, tweet.mentionedUsers])
+    
+    tweets_df = pd.DataFrame(attributes_container, columns=["User", "Date Created", "Tweet", "Mentioned"])
+    tweets_df = tweets_df[tweets_df["Tweet"].str.strip().str.len()>0]
+    tweets_df = tweets_df[tweets_df["Tweet"].str.contains("RTed, thanks!") == False]
+    
+    return tweets_df
+
+ schedule.every().day.at("10:30").do(econ_ra)
