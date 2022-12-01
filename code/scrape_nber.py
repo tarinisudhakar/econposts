799414f10eb81084782922be6a5b34818c6ad722
Tarini Sudhakar <tarinisudhakar@wireless-10-147-25-205.public.utexas.edu>
2022-12-01 13:39:14 -0600

    Scheduled scraper

--- code/scrape_nber.py
+++ code/scrape_nber.py
@@ -10,88 +10,92 @@ import schedule
 import time
 from collections import defaultdict
 
-def nber_list():
-    """Takes the RA listings on the NBER website that are not at the NBER and returns job title, NBER-sponsored researcher, institution, research field, and link to job posting"""
+class scheduleprint():  
 
-    response = requests.get("https://www.nber.org/career-resources/research-assistant-positions-not-nber")
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
-    text = soup.find_all("div", {"class": "page-header__intro-inner"})[0]
-    posts = text.find_all("p")
-       
-    clean_posts = posts[2:-2]
+    def nber_list(self):
+        """Takes the RA listings on the NBER website that are not at the NBER and returns job title, NBER-sponsored researcher, institution, research field, and link to job posting"""
 
-    joke = []
-    dic = defaultdict(list)
-    for i, v in enumerate(clean_posts): 
-        if v.contents[0] != '\xa0' and not str(v.contents[0]).startswith('<em><!--'):
-            pure = v.text.split('\n')
-            link = v.a["href"] if v.a["href"] else "linkmissing"
-            dic['JobTitle'].append(pure[0])
-            dic['Researcher'].append([item for item in pure if 'Sponsoring' in item][0] if [item for item in pure if 'Sponsoring' in item] else 'SponsoringMissing')
-            dic['Institution'].append([item for item in pure if 'Institution' in item][0] if [item for item in pure if 'Institution' in item] else 'InstitutionMissing')
-            dic['FieldofResearch'].append([item for item in pure if 'Field' in item][0] if [item for item in pure if 'Field' in item] else 'FieldMissing')
-            dic['JobLink'].append(link)
-            pure.append(link)
-            joke.append(pure)
-    
-    return dic 
+        response = requests.get("https://www.nber.org/career-resources/research-assistant-positions-not-nber")
+        html = response.text
+        soup = BeautifulSoup(html, "html.parser")
+        text = soup.find_all("div", {"class": "page-header__intro-inner"})[0]
+        posts = text.find_all("p")
+           
+        clean_posts = posts[2:-2]
 
-def posts(dic):
-    """Cleans the RA listings on the NBER website that are not at the NBER and returns job title, NBER-sponsored researcher, institution, research field, and link to job posting"""
+        joke = []
+        dic = defaultdict(list)
+        for i, v in enumerate(clean_posts): 
+            if v.contents[0] != '\xa0' and not str(v.contents[0]).startswith('<em><!--'):
+                pure = v.text.split('\n')
+                link = v.a["href"] if v.a["href"] else "linkmissing"
+                dic['JobTitle'].append(pure[0])
+                dic['Researcher'].append([item for item in pure if 'Sponsoring' in item][0] if [item for item in pure if 'Sponsoring' in item] else 'SponsoringMissing')
+                dic['Institution'].append([item for item in pure if 'Institution' in item][0] if [item for item in pure if 'Institution' in item] else 'InstitutionMissing')
+                dic['FieldofResearch'].append([item for item in pure if 'Field' in item][0] if [item for item in pure if 'Field' in item] else 'FieldMissing')
+                dic['JobLink'].append(link)
+                pure.append(link)
+                joke.append(pure)
+        
+        
+        df = pd.DataFrame(dic, columns =['JobTitle', 'Researcher', 'Institution', 'FieldofResearch', 'JobLink'])
+        
+        def split_util(x):
+            return x.split(':')[1].strip() if x.find(':') > 0 else ' '.join(x.split()[-2:])
 
-    df = pd.DataFrame(dic, columns =['JobTitle', 'Researcher', 'Institution', 'FieldofResearch', 'JobLink'])
-    
-    def split_util(x):
-        return x.split(':')[1].strip() if x.find(':') > 0 else ' '.join(x.split()[-2:])
-
-    df['Researcher'] = df['Researcher'].astype(str).apply(lambda row: split_util(row))
-    df['Institution'] = df['Institution'].astype(str).apply(lambda row: split_util(row))
-    df['FieldofResearch'] = df['FieldofResearch'].astype(str).apply(lambda row: split_util(row))   
-    
-    return df
+        df['Researcher'] = df['Researcher'].astype(str).apply(lambda row: split_util(row))
+        df['Institution'] = df['Institution'].astype(str).apply(lambda row: split_util(row))
+        df['FieldofResearch'] = df['FieldofResearch'].astype(str).apply(lambda row: split_util(row))   
 
 
-def upload_gsheets(df):
-    """Uploading to Google Sheets""" 
-    INPUT_DIR = "code"
-    INPUT_PATH = os.path.join(INPUT_DIR, "econpostscred.json")
+    #def upload_gsheets(df):
+        """Uploading to Google Sheets""" 
+        INPUT_DIR = "code"
+        INPUT_PATH = os.path.join(INPUT_DIR, "econpostscred.json")
 
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        INPUT_PATH, scope)
-    gc = gspread.authorize(credentials)
-
-
-    spreadsheet_key = '1X-R8QVhi2ngDTquGgpTwH_Vquj7DCFwROLhX2rQtEl4'
-    wks_name = 'nber'
-    #d2g.upload(df, spreadsheet_key, wks_name, credentials=credentials, row_names=True)
-
-    sheet = client.open('https://docs.google.com/spreadsheets/d/1X-R8QVhi2ngDTquGgpTwH_Vquj7DCFwROLhX2rQtEl4/edit#gid=1146760227')
-    sheet_instance = sheet.get_worksheet(0)
-    sheet_instance.insert_rows(df.values.tolist()) 
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            INPUT_PATH, scope)
+        client = gspread.authorize(credentials)
 
 
-if __name__ == '__main__':
-    print('Starting to be cool!')
-    result = nber_list()
-    print('Length of data is ', len(result))
-    posts = posts(result)
-    print('Length of posts is ', len(posts)) 
-    upload_gsheets(posts)
-    print('Update completed...')
-    print('I am done!')
+        spreadsheet_key = '1X-R8QVhi2ngDTquGgpTwH_Vquj7DCFwROLhX2rQtEl4'
+        wks_name = 'nber'
+        #d2g.upload(df, spreadsheet_key, wks_name, credentials=credentials, row_names=True)
 
-# #     schedule.every().day.at("08:48").do(nber_list, result)
-# #     schedule.every().day.at("08:50").do(posts, posts)
-# #     schedule.every().day.at("08:52").do(upload_gsheets, posts)
+        sheet = client.open('Economics RA listings_NBER and econ_ra')
+        sheet_instance = sheet.get_worksheet(0)
+        sheet_instance.update([df.columns.values.tolist()] + df.values.tolist())
+        #sheet_instance.insert_rows(df.values.tolist()) 
 
-# # # # Loop so that the scheduling task
-# # # # keeps on running all time.
-# #     while True:
- 
-# # #     # Checks whether a scheduled task
-# # #     # is pending to run or not
-# #         schedule.run_pending()
-# #         time.sleep(1)
+
+#if __name__ == '__main__':
+    # print('Starting to be cool!')
+    # #result = nber_list()
+    # #print('Length of data is ', len(result))
+    # df = nber_list(data)
+    # print('Length of posts is ', len(df)) 
+    # upload_gsheets(df)
+    # print('Update completed...')
+    # print('I am done!')
+
+
+    #def schedule_a_print_job(self, type = "Days", interval = 30): 
+    #    schedule.every(interval).seconds.do(self.nber_list)
+    def schedule_a_print_job(self): 
+        schedule.every().day.at("08:50").do(self.nber_list)
+        #schedule.every().day.at("20:10").do(posts(dic))
+        #schedule.every().day.at("20:31").do(self.upload_gsheet)
+
+    # # Loop so that the scheduling task
+    # # keeps on running all time.
+        while True:
+     
+    #     # Checks whether a scheduled task
+    #     # is pending to run or not
+            schedule.run_pending()
+            time.sleep(1)
+
+run = scheduleprint()
+run.schedule_a_print_job()
--- code/scrape_twitter.py
+++ code/scrape_twitter.py
@@ -9,56 +9,79 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pdb 
 
 
-def econ_ra():
-    data = []
-    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('@econ_ra').get_items()):
-        if i>200:
-            break
-        data.append([tweet.date, tweet.user.username, tweet.content, tweet.url, tweet.id])
+class scheduleprint(): 
+    def econ_ra(self):
+        data = []
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper('@econ_ra').get_items()):
+            if i>50:
+                break
+            data.append([tweet.date, tweet.user.username, tweet.content, tweet.url, tweet.id])
 
-    print(data)
-    return data
+        #print(data)
+        #return data
 
-def tweets(data): 
-    tweets_df = pd.DataFrame(data, columns=["Date Created", "User", "Tweet", "Links", "Tweet ID"])
-    tweets_df = tweets_df[tweets_df["Tweet"].str.strip().str.len()>0]
-    tweets_df = tweets_df[tweets_df["Tweet"].str.contains("RTed") == False]
-    tweets_df = tweets_df.replace(r'\n',  ' ', regex=True)
+    #def tweets(data): 
+        tweets_df = pd.DataFrame(data, columns=["Date Created", "User", "Tweet", "Links", "Tweet ID"])
+        tweets_df = tweets_df[tweets_df["Tweet"].str.strip().str.len()>0]
+        tweets_df = tweets_df[tweets_df["Tweet"].str.contains("RTed") == False]
+        tweets_df = tweets_df.replace(r'\n',  ' ', regex=True)
+        tweets_df["Date Created"] = tweets_df["Date Created"].astype(str)
+        
+        print(tweets_df)
+        #return tweets_df
+
+    #def upload_gsheets(tweets_df):
+
+        INPUT_DIR = "code"
+        INPUT_PATH = os.path.join(INPUT_DIR, "econpostscred.json")
+
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            INPUT_PATH, scope)
+        gc = gspread.authorize(credentials)
+
+
+        spreadsheet_key = '1X-R8QVhi2ngDTquGgpTwH_Vquj7DCFwROLhX2rQtEl4'
+        wks_name = 'twitter'
+        #d2g.upload(tweets_df, spreadsheet_key, wks_name, credentials=credentials, row_names=True)
+        #wks_name.update([tweets_df.columns.values.tolist()] + tweets_df.values.tolist())
+
+        sheet = gc.open('Economics RA listings_NBER and econ_ra')
+        sheet_instance = sheet.get_worksheet(2)
+        sheet_instance.update([tweets_df.columns.values.tolist()] + tweets_df.values.tolist())
+        #sheet_instance.insert_rows(tweets_df.values.tolist()[1:])
+
+    #def schedule_a_print_job(self, type = "Secs", interval = 30): 
+    #    schedule.every(interval).seconds.do(self.econ_ra)
     
-    print(tweets_df)
-    return tweets_df
+    def schedule_a_print_job(self): 
+        schedule.every().day.at("08:50").do(self.econ_ra)
+        #schedule.every().day.at("20:10").do(posts(dic))
+        #schedule.every().day.at("20:31").do(self.upload_gsheet)
 
-def upload_gsheets(tweets_df):
+    # # Loop so that the scheduling task
+    # # keeps on running all time.
+        while True:
+     
+    #     # Checks whether a scheduled task
+    #     # is pending to run or not
+            schedule.run_pending()
+            time.sleep(1)
 
-    INPUT_DIR = "code"
-    INPUT_PATH = os.path.join(INPUT_DIR, "econpostscred.json")
-
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        INPUT_PATH, scope)
-    gc = gspread.authorize(credentials)
-
-
-    spreadsheet_key = '1X-R8QVhi2ngDTquGgpTwH_Vquj7DCFwROLhX2rQtEl4'
-    wks_name = 'twitter'
-    #d2g.upload(tweets_df, spreadsheet_key, wks_name, credentials=credentials, row_names=True)
-    #wks_name.update([tweets_df.columns.values.tolist()] + tweets_df.values.tolist())
-
-    sheet = client.open('https://docs.google.com/spreadsheets/d/1X-R8QVhi2ngDTquGgpTwH_Vquj7DCFwROLhX2rQtEl4/edit#gid=648633538')
-    sheet_instance = sheet.get_worksheet(1)
-    sheet_instance.insert_rows(tweets_df.values.tolist())
+run = scheduleprint()
+run.schedule_a_print_job()
 
 
-if __name__ == '__main__':
-    print('Starting to be cool!')
-    results = econ_ra()
-    print('len is ', len(results))
-    tweets = tweets(results)
-    print('len res_tweets is ', len(tweets)) 
-    upload_gsheets(tweets)
-    print('Update completed...')
-    print('I am done!')
+# if __name__ == '__main__':
+#     print('Starting to be cool!')
+#     results = econ_ra()
+#     print('len is ', len(results))
+#     tweets = tweets(results)
+#     print('len res_tweets is ', len(tweets)) 
+#     upload_gsheets(tweets)
+#     print('Update completed...')
+#     print('I am done!')
 
 #     schedule.every().day.at("12:13").do(econ_ra)
 #     schedule.every().day.at("12:15").do(tweets)
